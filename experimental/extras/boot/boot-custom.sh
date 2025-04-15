d5354d5134b4f9b850c870f1d2f803652c6acd21
EnriqueWood <ewoodg@hotmail.com>
2025-04-14 23:59:13 -0400

    Improve failsafe script

--- experimental/extras/boot/boot-custom.sh
+++ experimental/extras/boot/boot-custom.sh
@@ -1,64 +1,81 @@
 #!/bin/bash
 
-AVOID_AUTO_RESTART_FLAG='/boot/noautorestart'
-
-MAX_BOOT_TIME_SECONDS=40
-SAMPLE_IMAGE_WIDTH=100
-SAMPLE_IMAGE_HEIGHT=100
-LOG_FILE="/tmp/boot-reboot.log"
+AVOID_AUTO_RESTART_FLAG='/userdata/noautorestart'
+SAMPLE_IMAGE_WIDTH=10
+SAMPLE_IMAGE_HEIGHT=10
+LOG_FILE="/tmp/boot-failsafe.log"
+START_BOOT_CHECK_TIME=50
+MAX_BOOT_TIME=180
+INTERVAL_SLEEP_TIME=10
+START_TIME=$(date +%s)
+RGB_FILE="/tmp/screen.rgb"
+export DISPLAY=:0
 
 log() {
-  echo "$@"
-  echo "$(date "+%F %H-%M-%S.%3N") [boot-custom] $@" >> "$LOG_FILE"
+  local now=$(date +%s)
+  local elapsed=$((now - START_TIME))
+  echo "[${elapsed}s] $@"
+  echo "$(date "+%F %H-%M-%S.%3N") [${elapsed}s] [boot-custom] $@" >> "${LOG_FILE}"
+}
+
+screenshot() {
+  rm -f "$RGB_FILE"
+  timeout --signal=SIGTERM --kill-after=1s 2s ffmpeg -f x11grab -video_size ${SAMPLE_IMAGE_WIDTH}x${SAMPLE_IMAGE_HEIGHT} -i $DISPLAY -frames:v 1 \
+             -f rawvideo -pix_fmt rgb24 "$RGB_FILE" -loglevel error >> "$LOG_FILE" 2>&1
 }
 
 (
-  log "Started" >> "$LOGFILE"
+  log "/boot/boot-custom.sh execution started"
 
-  sleep $MAX_BOOT_TIME_SECONDS
+  sleep $START_BOOT_CHECK_TIME
 
-  export DISPLAY=:0
-  RGB_FILE="/tmp/screen.rgb"
-  rm -f "$RGB_FILE"
+  for ((t=$START_BOOT_CHECK_TIME; t<=$MAX_BOOT_TIME; t+=$INTERVAL_SLEEP_TIME)); do
+    rm -f "$RGB_FILE"
+    sleep $INTERVAL_SLEEP_TIME
 
-  log 'Capturing screenshot'
+    log "Attempting new screenshot with DISPLAY=:0"
+    screenshot
 
-  ffmpeg -f x11grab -video_size ${SAMPLE_IMAGE_WIDTH}x${SAMPLE_IMAGE_HEIGHT} -i :0 -frames:v 1 \
-         -f rawvideo -pix_fmt rgb24 "$RGB_FILE" -loglevel error
-
-  if [ ! -f "$RGB_FILE" ]; then
-    log 'ERROR: Screen could not be captured'
-    exit 2
-  fi
-
-  log 'Screenshot captured'
-
-  found_color=0
-  while IFS= read -r -n3 -d '' pixel; do
-    R=$(printf "%d" "'${pixel:0:1}")
-    G=$(printf "%d" "'${pixel:1:1}")
-    B=$(printf "%d" "'${pixel:2:1}")
-
-    if ! { [ "$R" -eq 0 ] && [ "$G" -eq 0 ] && [ "$B" -eq 0 ]; } && # it's not a black pixel
-       ! { [ "$R" -eq 255 ] && [ "$G" -eq 255 ] && [ "$B" -eq 255 ]; }; then # or a white pixel
-      found_color=1
-      log "Non B/W color detected: R=$R G=$G B=$B"
-      break
-    else
-      log "Processed color: R=$R G=$G B=$B"
+    if [ ! -f "$RGB_FILE" ]; then
+      log "ERROR: Screenshot could not be captured, attempting with DISPLAY=:0.0"
+      export DISPLAY=:0.0
+      screenshot
+      if [ ! -f "$RGB_FILE" ]; then
+        sleep 2
+        log "ERROR: Screenshot could not be captured with DISPLAY=:0.0"
+        continue
+      fi
     fi
-  done < "$RGB_FILE"
 
-  if [ "$found_color" -eq 1 ]; then
-    log 'Boot process ok!'
-    exit 0
+    log "Screenshot captured"
+
+    found_color=0
+    while IFS= read -r -n3 -d '' pixel; do
+      R=$(printf "%d" "'${pixel:0:1}")
+      G=$(printf "%d" "'${pixel:1:1}")
+      B=$(printf "%d" "'${pixel:2:1}")
+
+      if ! { [ "$R" -eq 0 ] && [ "$G" -eq 0 ] && [ "$B" -eq 0 ]; } &&
+         ! { [ "$R" -eq 255 ] && [ "$G" -eq 255 ] && [ "$B" -eq 255 ]; }; then
+        found_color=1
+        log "Colored pixel detected: R=$R G=$G B=$B"
+        break
+      fi
+    done < "$RGB_FILE"
+
+    if [ "$found_color" -eq 1 ]; then
+      log "Boot process successful. Exiting failsafe check."
+      exit 0
+    else
+      log "No color detected, continuing checks..."
+    fi
+  done
+
+  if [ ! -f "$AVOID_AUTO_RESTART_FLAG" ]; then
+    log "Boot was unsuccessful, restarting system (create $AVOID_AUTO_RESTART_FLAG to avoid auto-restart)"
+    cp "$LOG_FILE" "/userdata/system/logs/boot-failsafe-$(date "+%F_%H-%M-%S.%3N").log"
+    reboot
   else
-    # if the file defined in AVOID_AUTO_RESTART_FLAG is found, auto-restarts will be inhibited
-    if [ ! -f "$AVOID_AUTO_RESTART_FLAG" ]; then
-      log "Boot was unsuccessful, restarting system (create a file in $AVOID_AUTO_RESTART_FLAG to avoid auto-restarting)"
-      reboot
-    else
-      log "Boot was unsuccessful, but auto-restarting mechanism inhibited by the presence of $AVOID_AUTO_RESTART_FLAG file"
-    fi
+    log "Boot was unsuccessful, but auto-restart inhibited by presence of $AVOID_AUTO_RESTART_FLAG"
   fi
 ) &
